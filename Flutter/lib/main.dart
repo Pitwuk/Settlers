1efa59b1200a9a97e42837275d7d19f4ec6dfd2f
Pitwuk <45837428+Pitwuk@users.noreply.github.com>
2020-04-13 15:00:56 -0400

    Improved the flutter menus up to num players

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -14,6 +14,7 @@ const colors = {
   'f': Color(0xff063300),
   'r': Color(0xffc4bf98)
 };
+
 void main() => runApp(Menu());
 
 class Menu extends StatelessWidget {
@@ -22,14 +23,23 @@ class Menu extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => StartMenu(),
-        '/singleplayer': null,
-        '/multiplayer': (context) => MultiplayerMenu()
+        '/singleplayer': (context) => SingleplayerMenu(),
+        '/multiplayer': (context) => MultiplayerMenu(),
+        '/local': (context) => NumPlayersMenu(),
+        '/online': (context) => OnlineMenu(),
+        '/names': (context) => InputNames()
       },
     );
   }
 }
 
 class StartMenu extends StatelessWidget {
+  var buttons = [
+    'Singleplayer',
+    '/singleplayer',
+    'Multiplayer',
+    '/multiplayer'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,65 +58,143 @@ class StartMenu extends StatelessWidget {
                   child: (Text('Welcome to The Settlers of Catan',
                       textAlign: TextAlign.center,
                       style: TextStyle(fontSize: 40.0))))),
-          MenuButtons()
+          MenuButtons(buttons)
         ],
       ),
     );
   }
 }
 
-class MenuButtons extends StatefulWidget {
+class MultiplayerMenu extends StatelessWidget {
+  var buttons = ['Local', '/local', 'Online', '/online'];
   @override
-  _MenuState createState() => _MenuState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colors['beige'],
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 7.0,
+                color: colors['brown'],
+              ),
+            ),
+          ),
+          MenuButtons(buttons)
+        ],
+      ),
+    );
+  }
+}
+
+class SingleplayerMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Singleplayer Not Yet Supported',
+            style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
+class OnlineMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Online Not Yet Supported',
+            style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
+class NumPlayersMenu extends StatelessWidget {
+  var buttons = ['1', '/names', '2', '/names', '3', '/names', '4', '/names'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colors['beige'],
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 7.0,
+                color: colors['brown'],
+              ),
+            ),
+          ),
+          MenuButtons(buttons)
+        ],
+      ),
+    );
+  }
+}
+
+class InputNames extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Can\'t Enter Names Yet',
+            style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
+class MenuButtons extends StatefulWidget {
+  var buttons;
+  MenuButtons(this.buttons);
+  @override
+  _MenuState createState() => _MenuState(buttons);
 }
 
 class _MenuState extends State<MenuButtons> {
+  var buttons;
+  _MenuState(this.buttons);
+
   void _showNextScreen(route) {
     Navigator.of(context).pushNamed(route);
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> button_widgets = new List(buttons.length / 2);
+    for (int i = 0; i < buttons.length - 1; i += 2) {
+      print(i);
+      print((i / 2).round());
+      button_widgets[(i / 2).round()] =
+          menuButton(context, buttons[i], 200, 100, buttons[i + 1]);
+    }
     return Center(
-        child: Stack(children: <Widget>[
-      menuButton(context, 'Singleplayer', -0.22, 0, .16, .16, '/singleplayer'),
-      menuButton(context, 'Multiplayer', 0.22, 0, .16, .16, '/multiplayer')
-    ]));
+        child: Wrap(spacing: 10, runSpacing: 10, children: button_widgets));
   }
 
-  Widget menuButton(BuildContext context, txt, x, y, width, height, route) {
-    return Align(
-        alignment: Alignment(x, y),
-        child: FractionallySizedBox(
-            widthFactor: width,
-            heightFactor: height,
-            child: Container(
-                child: MouseRegion(
-                    onHover: (event) {
-                      appContainer.style.cursor = 'pointer';
-                    },
-                    onExit: (event) {
-                      appContainer.style.cursor = 'default';
-                    },
-                    child: FlatButton(
-                      color: colors['brown'],
-                      splashColor: colors['beige'],
-                      hoverColor: colors['s'],
-                      onPressed: () => _showNextScreen(route),
-                      child: AutoSizeText(txt,
-                          style: TextStyle(fontSize: 60.0, color: Colors.white),
-                          maxLines: 1),
-                    )))));
-  }
-}
-
-class MultiplayerMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Woo!', style: Theme.of(context).textTheme.headline2),
-      ),
-    );
+  Widget menuButton(BuildContext context, txt, width, height, route) {
+    return SizedBox(
+        width: width,
+        height: height,
+        child: Container(
+            child: MouseRegion(
+                onHover: (event) {
+                  appContainer.style.cursor = 'pointer';
+                },
+                onExit: (event) {
+                  appContainer.style.cursor = 'default';
+                },
+                child: FlatButton(
+                  color: colors['brown'],
+                  splashColor: colors['beige'],
+                  hoverColor: colors['s'],
+                  onPressed: () => _showNextScreen(route),
+                  child: AutoSizeText(txt,
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      maxLines: 1),
+                ))));
   }
 }
